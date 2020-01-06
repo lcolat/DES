c13d7f8b33bb67751ff7fff676296f307b958a4e
Malivert <christopher.malivert@orange.fr>
2020-01-06 16:53:55 +0100

    first commit, main et MatZero init

---
+++ main.py
@@ -0,0 +1,16 @@
+from array import array
+
+
+def main():
+    fichier = open("TP-DES\Messages\Clef_de_1.txt", "r")
+    cleD = fichier.readline()
+    fichier.close()
+    cleF = ""
+    print(len(cleD))
+    print(cleD)
+    i = 0
+    for i in range(0, 63) :
+        if i != 7 and i != 15 and i != 23 and i != 31 and i != 39 and i != 47 and i != 55:
+            cleF = cleF + cleD[i]
+    print(cleF)
+main()
